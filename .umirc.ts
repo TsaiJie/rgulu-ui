f780e316bc290ba85e6b4fdef176031bb88176bb
tsai <mengjietsai@163.com>
2020-08-21 22:40:10 +0800

    修改仓库名字

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'rgulu-ui',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
+  // more config: https://d.umijs.org/config
+  // git@github.com:<USERNAME>/<REPO>.git
+  base: 'git@github.com:TsaiJie/rgulu-ui.git/',
+  publicPath: 'git@github.com:TsaiJie/rgulu-ui.git/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
 });
