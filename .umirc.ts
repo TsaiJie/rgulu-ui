be80228cc85631a9b0aa5ae8dcf4234be3ecfc98
tsai <mengjietsai@163.com>
2020-08-22 08:00:52 +0800

    up3

--- .umirc.ts
+++ .umirc.ts
@@ -6,10 +6,8 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
+  outputPath: 'docs',
   // more config: https://d.umijs.org/config
-  // more config: https://d.umijs.org/config
-  // git@github.com:<USERNAME>/<REPO>.git
   base: '/rgulu-ui',
   publicPath: '/rgulu-ui/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
