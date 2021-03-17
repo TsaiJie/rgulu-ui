6f2a787b20b57d0763b0bc37a32a1c666e8bad30
tsai <mengjietsai@163.com>
2021-03-17 14:41:58 +0800

    表单校验器支持异步操作

--- src/components/Form/index.md
+++ src/components/Form/index.md
@@ -6,6 +6,25 @@
 import React, { useState, Fragment, useCallback } from 'react';
 import { Form, Button, FormValue, Validator, noError } from 'rgulu-ui';
 
+const usernames = ['frank', 'jack', 'alice', 'bob'];
+const checkUserName = (
+  username: string,
+  succeed: () => void,
+  fail: () => void,
+) => {
+  setTimeout(() => {
+    console.log(
+      '我现在知道用户名是否存在',
+      username,
+      usernames.indexOf(username),
+    );
+    if (usernames.indexOf(username) >= 0) {
+      succeed();
+    } else {
+      fail();
+    }
+  }, 1000);
+};
 export default () => {
   const [formData, setFormData] = useState<FormValue>({
     username: '',
@@ -19,25 +38,42 @@ export default () => {
   const rules = [
     { key: 'username', required: true },
     { key: 'username', minLength: 8, maxLength: 16 },
+    {
+      key: 'username',
+      validator: {
+        name: 'unique',
+        validate(username: string) {
+          console.log('有人调用了validate');
+          return new Promise<void>((resolve, reject) => {
+            checkUserName(username, resolve, reject);
+          });
+        },
+      },
+    },
     { key: 'username', pattern: /^[A-Za-z0-9]+$/ },
     { key: 'password', required: true },
   ];
 
-  const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    const errors: FormErrors = Validator(formData, rules);
-    if (noError(errors)) {
-      console.log(errors);
-    }
-    setErrors(errors);
-  }, []);
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      Validator(formData, rules, errors => {
+        console.log('errors', errors);
+        if (noError(errors)) {
+          console.log(errors);
+        }
+        setErrors(errors);
+      });
+    },
+    [formData],
+  );
   const onChange = useCallback(newValue => {
-    const errors: FormErrors = Validator(newValue, rules);
-    setFormData(newValue);
-    setErrors(errors);
+    // const errors: FormErrors = Validator(newValue, rules);
+    setFormData({ ...newValue });
+    // setErrors(errors);
   }, []);
-  console.log(FormValue);
   return (
     <Fragment>
+      {JSON.stringify(errors)}
       <Form
         value={formData}
         fields={fields}
--- src/components/Form/validator.tsx
+++ src/components/Form/validator.tsx
@@ -6,45 +6,103 @@ interface FormRule {
   minLength?: number;
   maxLength?: number;
   pattern?: RegExp;
+  validator?: {
+    name: string;
+    validate: (value: string) => Promise<void>;
+  };
 }
 
-interface FormErrors {
-  [k: string]: string[];
+interface OneError {
+  message: string;
+  promise?: Promise<any>;
 }
 
 type FormRules = Array<FormRule>;
 
 function isEmpty(value: unknown) {
+  console.log('isEmpty', value);
   return value === undefined || value === null || value === '';
 }
+
 export function noError(errors: any) {
   return Object.keys(errors).length === 0;
 }
 
-const Validator = (formValue: FormValue, rules: FormRules): FormErrors => {
-  let errors: FormErrors = {};
-  const addError = (key: string, message: string) => {
+const Validator = (
+  formValue: FormValue,
+  rules: FormRules,
+  callback: (errors: any) => void,
+) => {
+  let errors: any = {};
+  const addError = (key: string, error: OneError) => {
     if (errors[key] === undefined) {
       errors[key] = [];
     }
-    errors[key].push(message);
+    errors[key].push(error);
   };
   rules.map(rule => {
     const value = formValue[rule.key];
+    if (rule.validator) {
+      //自定义的校验器
+      console.log(value, formValue);
+      const promise = rule.validator.validate(value);
+      addError(rule.key, { message: '用户名已存在', promise });
+    }
     if (rule.required && isEmpty(value)) {
-      addError(rule.key, '必填');
+      console.log('required', value);
+      addError(rule.key, { message: '必填' });
     }
     if (rule.minLength && !isEmpty(value) && value!.length < rule.minLength) {
-      addError(rule.key, '太短');
+      addError(rule.key, { message: '太短了' });
     }
     if (rule.maxLength && !isEmpty(value) && value!.length > rule.maxLength) {
-      addError(rule.key, '太长');
+      addError(rule.key, { message: '太长了' });
     }
     if (rule.pattern && !rule.pattern.test(value)) {
-      addError(rule.key, '格式不合法');
+      addError(rule.key, { message: '格式不正确' });
     }
   });
-  return errors;
+  const promiseList = flat(Object.values(errors))
+    .filter(item => item.promise)
+    .map(item => item.promise);
+  Promise.all(promiseList)
+    .then(() => {
+      const newErrors = Object.keys(errors).map<[string, string[]]>(key => {
+        // key username/ password
+        // errors[key] / []
+        return [key, errors[key].map((item: OneError) => item.message)];
+      });
+      callback(formEntries(newErrors));
+    })
+    .catch(() => {
+      const newErrors = Object.keys(errors).map<[string, string[]]>(key => {
+        // key username/ password
+        // errors[key] / []
+        return [key, errors[key].map((item: OneError) => item.message)];
+      });
+      callback(formEntries(newErrors));
+    });
 };
 
 export default Validator;
+
+function flat(array: Array<any>) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result.push(...array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+
+function formEntries(array: Array<[string, string[]]>) {
+  const result: { [key: string]: string[] } = {};
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    result[array[i][0]] = array[i][1];
+  }
+  return result;
+}
