269088821ec3aecf93532fb347cc155ee6982ae0
tsai <mengjietsai@163.com>
2021-04-02 15:33:21 +0800

    计算自定义滚动条的高度

--- src/components/scroll/scroll.tsx
+++ src/components/scroll/scroll.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
-import { HTMLAttributes } from 'react';
+import {
+  HTMLAttributes,
+  UIEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { scopedClassMaker } from '@/helper/classes';
 import './scroll.scss';
 import { scrollbarWidth } from '@/components/scroll/scrollbarWidth';
@@ -10,10 +16,31 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 const Scroll: React.FunctionComponent<Props> = props => {
   const { children, ...rest } = props;
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [barHeight, setBarHeight] = useState(0);
+  const onScroll: UIEventHandler = e => {};
+  useEffect(() => {
+    // mounted 挂载的时候计算滚动条的高度
+    // 整个区域滚动的高度
+    const scrollHeight = containerRef.current!.scrollHeight;
+    // 可视范围的高度
+    const viewHeight = containerRef.current!.getBoundingClientRect().height;
+    // x/y = x1/y1的思想
+    setBarHeight((viewHeight * viewHeight) / scrollHeight);
+  }, []);
+
   return (
     <div className={sc('')} {...rest}>
-      <div className={sc('inner')} style={{ right: -scrollbarWidth() }}>
+      <div
+        className={sc('inner')}
+        style={{ right: -scrollbarWidth() }}
+        onScroll={onScroll}
+        ref={containerRef}
+      >
         {children}
+      </div>
+      <div className={sc('track')}>
+        <div className={sc('bar')} style={{ height: barHeight }} />
       </div>
     </div>
   );
